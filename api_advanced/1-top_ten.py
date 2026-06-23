b95dbb13b6ae44519d1a1d6e1fad0ec72779e666
Ajang Biar <a.biar1@alustudent.com>
2026-06-23 21:37:07 +0200

    completed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,16 +7,18 @@ def top_ten(subreddit):
     """Print the titles of the first 10 hot posts for a given subreddit."""
     headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    
-    # Don't follow redirects (handles invalid subreddits)
+
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         try:
             data = response.json()
             posts = data.get('data', {}).get('children', [])
-            for post in posts[:10]:
-                print(post.get('data', {}).get('title'))
+            if posts:
+                for post in posts[:10]:
+                    print(post.get('data', {}).get('title'))
+            else:
+                print(None)
         except:
             print(None)
     else:
