f5c17bcfde01020fdce10e161882481f8e0e5d57
Ajang Biar <a.biar1@alustudent.com>
2026-06-23 21:17:57 +0200

    completed task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Function to query Reddit API and get top 10 hot posts
+Module for querying Reddit API to get top 10 hot posts
 """
 import requests
 
@@ -9,39 +9,30 @@ def top_ten(subreddit):
     """
     Queries the Reddit API and prints the titles of the first 10 hot posts
     for a given subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit to query
-    
+
     Returns:
         None: Prints titles or None if invalid subreddit
     """
-    # Set up the URL and headers
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {
-        'User-Agent': 'MyRedditBot/1.0 (com.example.mybot)'
+        'User-Agent': 'MyRedditBot/1.0'
     }
     params = {
         'limit': 10
     }
-    
+
     try:
-        # Make the request with redirects disabled
-        response = requests.get(url, headers=headers, params=params, 
-                               allow_redirects=False)
-        
-        # Check if the request was successful and not a redirect
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            posts = data['data']['children']
-            
-            # Print the titles of the first 10 posts
+            posts = data.get('data', {}).get('children', [])
             for post in posts:
-                print(post['data']['title'])
+                print(post.get('data', {}).get('title'))
         else:
-            # Invalid subreddit or other error
             print(None)
-            
-    except requests.exceptions.RequestException:
-        # Network error or other exception
+    except requests.RequestException:
         print(None)
