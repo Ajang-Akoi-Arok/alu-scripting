ea15cee1bccfd20e55e145ee14dce51dca438979
Ajang Biar <a.biar1@alustudent.com>
2026-06-23 21:02:42 +0200

    correction

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
-
+"""
+Function to query Reddit API and get top 10 hot posts
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query
+    
+    Returns:
+        None: Prints titles or None if invalid subreddit
+    """
+    # Set up the URL and headers
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        'User-Agent': 'MyRedditBot/1.0 (com.example.mybot)'
+    }
+    params = {
+        'limit': 10
+    }
+    
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
-    except Exception:
+        # Make the request with redirects disabled
+        response = requests.get(url, headers=headers, params=params, 
+                               allow_redirects=False)
+        
+        # Check if the request was successful and not a redirect
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            
+            # Print the titles of the first 10 posts
+            for post in posts:
+                print(post['data']['title'])
+        else:
+            # Invalid subreddit or other error
+            print(None)
+            
+    except requests.exceptions.RequestException:
+        # Network error or other exception
         print(None)
